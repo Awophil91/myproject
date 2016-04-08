9ebff3f21c0ab39509aec52c3d2eae1c640eda80
Muyiwa <muyiwaawoniyi@yahoo.com>
2016-04-08 22:00:20 +0100

    Update

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -5,31 +5,49 @@
 | Application Routes
 |--------------------------------------------------------------------------
 |
-| Here is where you can register all of the routes for an application.
-| It's a breeze. Simply tell Laravel the URIs it should respond to
-| and give it the controller to call when that URI is requested.
+| This route group applies the "web" middleware group to every route
+| it contains. The "web" middleware group is defined in your HTTP
+| kernel and includes session state, CSRF protection, and more.
 |
 */
 
-/**Route::get('/', function () {
-    return view('welcome');
-});*/
+use App\Task;
+use Illuminate\Http\Request;
 
+Route::group(['middleware' => ['web']], function () {
+    /**
+     * Show Task Dashboard
+     */
+    Route::get('/', function () {
+        return view('tasks', [
+            'tasks' => Task::orderBy('created_at', 'asc')->get()
+        ]);
+    });
 
-Route::get('/', function () {
-    //
-});
+    /**
+     * Add New Task
+     */
+    Route::post('/task', function (Request $request) {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|max:255',
+        ]);
 
-/**
- * Add A New Task
- */
-Route::post('/task', function (Request $request) {
-    //
-});
+        if ($validator->fails()) {
+            return redirect('/')->withInput()->withErrors($validator);
+        }
 
-/**
- * Delete An Existing Task
- */
-Route::delete('/task/{id}', function ($id) {
-    //
+        $task = new Task;
+        $task->name = $request->name;
+        $task->save();
+
+        return redirect('/');
+    });
+
+    /**
+     * Delete Task
+     */
+    Route::delete('/task/{id}', function ($id) {
+        Task::findOrFail($id)->delete();
+        return redirect('/');
+    });
 });
---
+++ resources/views/tasks.blade.php
@@ -0,0 +1,89 @@
+@extends('layouts.app')
+
+@section('content')
+        <div class="col-sm-offset-2 col-sm-8">
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    New Task
+                </div>
+
+                <div class="panel-body">
+                    <!-- Display Validation Errors -->
+                    <!--@include('common.errors')-->
+                    @if (count($errors) > 0)
+                        <!-- Form Error List -->
+                        <div class="alert alert-danger">
+                            <strong>Whoops! Something went wrong!</strong>
+
+                            <br><br>
+
+                            <ul>
+                                @foreach ($errors->all() as $error)
+                                    <li>{{ $error }}</li>
+                                @endforeach
+                            </ul>
+                        </div>
+                        @endif
+
+                    <!-- New Task Form -->
+                    <form action="/task" method="POST" class="form-horizontal">
+                        {{ csrf_field() }}
+
+                        <!-- Task Name -->
+                        <div class="form-group">
+                            <label for="task-name" class="col-sm-3 control-label">Task</label>
+                            <div class="col-sm-6">
+                                <input type="text" name="name" id="task-name" class="form-control" value="{{ old('task') }}">
+                            </div>
+                        </div>
+
+                        <!-- Add Task Button -->
+                        <div class="form-group">
+                            <div class="col-sm-offset-3 col-sm-6">
+                                <button type="submit" class="btn btn-default">
+                                    <i class="fa fa-btn fa-plus"></i>ADD
+                                </button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+
+            <!-- Current Tasks -->
+            @if (count($tasks) > 0)
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        Current Tasks
+                    </div>
+
+                    <div class="panel-body">
+                        <table class="table table-striped task-table">
+                            <thead>
+                            <th>Task</th>
+                            <th>&nbsp;</th>
+                            </thead>
+                            <tbody>
+                            @foreach ($tasks as $task)
+                                <tr>
+                                    <td class="table-text"><div>{{ $task->name }}</div></td>
+
+                                    <!-- Task Delete Button -->
+                                    <td>
+                                        <form action="/task/{{ $task->id }}" method="POST">
+                                            {{ csrf_field() }}
+                                            {{ method_field('DELETE') }}
+
+                                            <button type="submit" class="btn btn-danger">
+                                                <i class="fa fa-btn fa-trash"></i>DELETE
+                                            </button>
+                                        </form>
+                                    </td>
+                                </tr>
+                            @endforeach
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            @endif
+        </div>
+@endsection
