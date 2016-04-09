ec94ae2dd22ffeb23c26efb1169a6060dbb2b7ab
Muyiwa <muyiwaawoniyi@yahoo.com>
2016-04-09 08:31:18 +0100

    Update

--- resources/views/tasks.blade.php
+++ resources/views/tasks.blade.php
@@ -9,21 +9,7 @@
 
                 <div class="panel-body">
                     <!-- Display Validation Errors -->
-                    <!--@include('common.errors')-->
-                    @if (count($errors) > 0)
-                        <!-- Form Error List -->
-                        <div class="alert alert-danger">
-                            <strong>Whoops! Something went wrong!</strong>
-
-                            <br><br>
-
-                            <ul>
-                                @foreach ($errors->all() as $error)
-                                    <li>{{ $error }}</li>
-                                @endforeach
-                            </ul>
-                        </div>
-                        @endif
+                    @include('common.errors')
 
                     <!-- New Task Form -->
                     <form action="/task" method="POST" class="form-horizontal">
