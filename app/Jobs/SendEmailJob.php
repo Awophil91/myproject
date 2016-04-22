10e64bbd9b56e2864a7f90b26a60aa7cc6978e02
Muyiwa <muyiwaawoniyi@yahoo.com>
2016-04-22 03:51:10 +0100

    full

---
+++ app/Jobs/SendEmailJob.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Manager\Jobs;
+
+use Illuminate\Contracts\Mail\Mailer;
+use Manager\Jobs\Job;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+
+
+class SendEmailJob extends Job implements ShouldQueue
+{
+    use InteractsWithQueue, SerializesModels;
+
+
+    protected $count;
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct($count)
+    {
+        $this->count=$count;
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle(Mailer $mailer)
+    {
+        $count=$this->count;
+        $data=array('theCount' => $count);
+
+        $mailer->send('emails.welcome', $data, function ($message)
+
+        {
+            $message->from('muyiwaawoniyi@yahoo.com', env('MAIL_NAME'));
+            $message->to('muyiwaawoniyi@yahoo.com')->subject('Welcome!');
+        });
+    }
+}
