84867746b2d90ee19852126ecd911ea7de187578
Muyiwa <muyiwaawoniyi@yahoo.com>
2016-04-29 05:45:54 +0100

    latest

--- app/Repos/TaskRepository.php
+++ app/Repos/TaskRepository.php
@@ -1,11 +1,22 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Muyiwa
- * Date: 4/29/2016
- * Time: 5:44 AM
- */
 
-class TaskRepository {
+namespace Manager\Repositories;
 
+use Manager\Models\Task;
+use Manager\Models\User;
+
+class TaskRepository
+{
+    /**
+     * Get all of the tasks for a given user.
+     *
+     * @param  User  $user
+     * @return Collection
+     */
+    public function forUser(User $user)
+    {
+        return Task::where('user_id', $user->id)
+            ->orderBy('created_at', 'asc')
+            ->get();
+    }
 }
